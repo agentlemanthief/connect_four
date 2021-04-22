3386e2349dbb1e9c70bca6003059c4cd00abe5c9
Nicholas Baldwin <nicholas.baldwin@icloud.com>
2021-04-22 21:54:07 +0100

    Added #player_turn and #play_game to ConnectFour class and Player class methods #ask_name, #ask_token and #validate?

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,14 +1,15 @@
 # Frozen_string_literal: true
 
-# Class describing the connect four game and methods for playing
-
 require_relative 'player'
 
+# Class describing the connect four game and methods for playing
 class ConnectFour
   attr_reader :board
 
   def initialize
     @board = Array.new(6) { Array.new(7, '_') }
+    @player_number_one = Player.new(1)
+    @player_number_two = Player.new(2)
   end
 
   def display
@@ -36,74 +37,74 @@ class ConnectFour
 
   def winning_conditions(token)
     @board[5][0] == token && @board[5][1] == token && @board[5][2] == token && @board[5][3] == token ||
-    @board[5][1] == token && @board[5][2] == token && @board[5][3] == token && @board[5][4] == token ||
-    @board[5][2] == token && @board[5][3] == token && @board[5][4] == token && @board[5][5] == token ||
-    @board[5][3] == token && @board[5][4] == token && @board[5][5] == token && @board[5][6] == token ||
-    @board[4][0] == token && @board[4][1] == token && @board[4][2] == token && @board[4][3] == token ||
-    @board[4][1] == token && @board[4][2] == token && @board[4][3] == token && @board[4][4] == token ||
-    @board[4][2] == token && @board[4][3] == token && @board[4][4] == token && @board[4][5] == token ||
-    @board[4][3] == token && @board[4][4] == token && @board[4][5] == token && @board[4][6] == token ||
-    @board[3][0] == token && @board[3][1] == token && @board[3][2] == token && @board[3][3] == token ||
-    @board[3][1] == token && @board[3][2] == token && @board[3][3] == token && @board[3][4] == token ||
-    @board[3][2] == token && @board[3][3] == token && @board[3][4] == token && @board[3][5] == token ||
-    @board[3][3] == token && @board[3][4] == token && @board[3][5] == token && @board[3][6] == token ||
-    @board[2][0] == token && @board[2][1] == token && @board[2][2] == token && @board[2][3] == token ||
-    @board[2][1] == token && @board[2][2] == token && @board[2][3] == token && @board[2][4] == token ||
-    @board[2][2] == token && @board[2][3] == token && @board[2][4] == token && @board[2][5] == token ||
-    @board[2][3] == token && @board[2][4] == token && @board[2][5] == token && @board[2][6] == token ||
-    @board[1][0] == token && @board[1][1] == token && @board[1][2] == token && @board[1][3] == token ||
-    @board[1][1] == token && @board[1][2] == token && @board[1][3] == token && @board[1][4] == token ||
-    @board[1][2] == token && @board[1][3] == token && @board[1][4] == token && @board[1][5] == token ||
-    @board[1][3] == token && @board[1][4] == token && @board[1][5] == token && @board[1][6] == token ||
-    @board[0][0] == token && @board[0][1] == token && @board[0][2] == token && @board[0][3] == token ||
-    @board[0][1] == token && @board[0][2] == token && @board[0][3] == token && @board[0][4] == token ||
-    @board[0][2] == token && @board[0][3] == token && @board[0][4] == token && @board[0][5] == token ||
-    @board[0][3] == token && @board[0][4] == token && @board[0][5] == token && @board[0][6] == token ||
-    @board[5][0] == token && @board[4][0] == token && @board[3][0] == token && @board[2][0] == token ||
-    @board[4][0] == token && @board[3][0] == token && @board[2][0] == token && @board[1][0] == token ||
-    @board[3][0] == token && @board[2][0] == token && @board[1][0] == token && @board[0][0] == token ||
-    @board[5][1] == token && @board[4][1] == token && @board[3][1] == token && @board[2][1] == token ||
-    @board[4][1] == token && @board[3][1] == token && @board[2][1] == token && @board[1][1] == token ||
-    @board[3][1] == token && @board[2][1] == token && @board[1][1] == token && @board[0][1] == token ||
-    @board[5][2] == token && @board[4][2] == token && @board[3][2] == token && @board[2][2] == token ||
-    @board[4][2] == token && @board[3][2] == token && @board[2][2] == token && @board[1][2] == token ||
-    @board[3][2] == token && @board[2][2] == token && @board[1][2] == token && @board[0][2] == token ||
-    @board[5][3] == token && @board[4][3] == token && @board[3][3] == token && @board[2][3] == token ||
-    @board[4][3] == token && @board[3][3] == token && @board[2][3] == token && @board[1][3] == token ||
-    @board[3][3] == token && @board[2][3] == token && @board[1][3] == token && @board[0][3] == token ||
-    @board[5][4] == token && @board[4][4] == token && @board[3][4] == token && @board[2][4] == token ||
-    @board[4][4] == token && @board[3][4] == token && @board[2][4] == token && @board[1][4] == token ||
-    @board[3][4] == token && @board[2][4] == token && @board[1][4] == token && @board[0][4] == token ||
-    @board[5][5] == token && @board[4][5] == token && @board[3][5] == token && @board[2][5] == token ||
-    @board[4][5] == token && @board[3][5] == token && @board[2][5] == token && @board[1][5] == token ||
-    @board[3][5] == token && @board[2][5] == token && @board[1][5] == token && @board[0][5] == token ||
-    @board[5][6] == token && @board[4][6] == token && @board[3][6] == token && @board[2][6] == token ||
-    @board[4][6] == token && @board[3][6] == token && @board[2][6] == token && @board[1][6] == token ||
-    @board[3][6] == token && @board[2][6] == token && @board[1][6] == token && @board[0][6] == token ||
-    @board[5][0] == token && @board[4][1] == token && @board[3][2] == token && @board[2][3] == token ||
-    @board[5][1] == token && @board[4][2] == token && @board[3][3] == token && @board[2][4] == token ||
-    @board[5][2] == token && @board[4][3] == token && @board[3][4] == token && @board[2][5] == token ||
-    @board[5][3] == token && @board[4][4] == token && @board[3][5] == token && @board[2][6] == token ||
-    @board[4][0] == token && @board[3][1] == token && @board[2][2] == token && @board[1][3] == token ||
-    @board[4][1] == token && @board[3][2] == token && @board[2][3] == token && @board[1][4] == token ||
-    @board[4][2] == token && @board[3][3] == token && @board[2][4] == token && @board[1][5] == token ||
-    @board[4][3] == token && @board[3][4] == token && @board[2][5] == token && @board[1][6] == token ||
-    @board[3][0] == token && @board[2][1] == token && @board[1][2] == token && @board[0][3] == token ||
-    @board[3][1] == token && @board[2][2] == token && @board[1][3] == token && @board[0][4] == token ||
-    @board[3][2] == token && @board[2][3] == token && @board[1][4] == token && @board[0][5] == token ||
-    @board[3][3] == token && @board[2][4] == token && @board[1][5] == token && @board[0][6] == token ||
-    @board[5][6] == token && @board[4][5] == token && @board[3][4] == token && @board[2][3] == token ||
-    @board[5][5] == token && @board[4][4] == token && @board[3][3] == token && @board[2][2] == token ||
-    @board[5][4] == token && @board[4][3] == token && @board[3][2] == token && @board[2][1] == token ||
-    @board[5][3] == token && @board[4][2] == token && @board[3][1] == token && @board[2][0] == token ||
-    @board[4][6] == token && @board[3][5] == token && @board[2][4] == token && @board[1][3] == token ||
-    @board[4][5] == token && @board[3][4] == token && @board[2][3] == token && @board[1][2] == token ||
-    @board[4][4] == token && @board[3][3] == token && @board[2][2] == token && @board[1][1] == token ||
-    @board[4][3] == token && @board[3][2] == token && @board[2][1] == token && @board[1][0] == token ||
-    @board[3][6] == token && @board[2][5] == token && @board[1][4] == token && @board[0][3] == token ||
-    @board[3][5] == token && @board[2][4] == token && @board[1][3] == token && @board[0][2] == token ||
-    @board[3][4] == token && @board[2][3] == token && @board[1][2] == token && @board[0][1] == token ||
-    @board[3][3] == token && @board[2][2] == token && @board[1][1] == token && @board[0][0] == token
+      @board[5][1] == token && @board[5][2] == token && @board[5][3] == token && @board[5][4] == token ||
+      @board[5][2] == token && @board[5][3] == token && @board[5][4] == token && @board[5][5] == token ||
+      @board[5][3] == token && @board[5][4] == token && @board[5][5] == token && @board[5][6] == token ||
+      @board[4][0] == token && @board[4][1] == token && @board[4][2] == token && @board[4][3] == token ||
+      @board[4][1] == token && @board[4][2] == token && @board[4][3] == token && @board[4][4] == token ||
+      @board[4][2] == token && @board[4][3] == token && @board[4][4] == token && @board[4][5] == token ||
+      @board[4][3] == token && @board[4][4] == token && @board[4][5] == token && @board[4][6] == token ||
+      @board[3][0] == token && @board[3][1] == token && @board[3][2] == token && @board[3][3] == token ||
+      @board[3][1] == token && @board[3][2] == token && @board[3][3] == token && @board[3][4] == token ||
+      @board[3][2] == token && @board[3][3] == token && @board[3][4] == token && @board[3][5] == token ||
+      @board[3][3] == token && @board[3][4] == token && @board[3][5] == token && @board[3][6] == token ||
+      @board[2][0] == token && @board[2][1] == token && @board[2][2] == token && @board[2][3] == token ||
+      @board[2][1] == token && @board[2][2] == token && @board[2][3] == token && @board[2][4] == token ||
+      @board[2][2] == token && @board[2][3] == token && @board[2][4] == token && @board[2][5] == token ||
+      @board[2][3] == token && @board[2][4] == token && @board[2][5] == token && @board[2][6] == token ||
+      @board[1][0] == token && @board[1][1] == token && @board[1][2] == token && @board[1][3] == token ||
+      @board[1][1] == token && @board[1][2] == token && @board[1][3] == token && @board[1][4] == token ||
+      @board[1][2] == token && @board[1][3] == token && @board[1][4] == token && @board[1][5] == token ||
+      @board[1][3] == token && @board[1][4] == token && @board[1][5] == token && @board[1][6] == token ||
+      @board[0][0] == token && @board[0][1] == token && @board[0][2] == token && @board[0][3] == token ||
+      @board[0][1] == token && @board[0][2] == token && @board[0][3] == token && @board[0][4] == token ||
+      @board[0][2] == token && @board[0][3] == token && @board[0][4] == token && @board[0][5] == token ||
+      @board[0][3] == token && @board[0][4] == token && @board[0][5] == token && @board[0][6] == token ||
+      @board[5][0] == token && @board[4][0] == token && @board[3][0] == token && @board[2][0] == token ||
+      @board[4][0] == token && @board[3][0] == token && @board[2][0] == token && @board[1][0] == token ||
+      @board[3][0] == token && @board[2][0] == token && @board[1][0] == token && @board[0][0] == token ||
+      @board[5][1] == token && @board[4][1] == token && @board[3][1] == token && @board[2][1] == token ||
+      @board[4][1] == token && @board[3][1] == token && @board[2][1] == token && @board[1][1] == token ||
+      @board[3][1] == token && @board[2][1] == token && @board[1][1] == token && @board[0][1] == token ||
+      @board[5][2] == token && @board[4][2] == token && @board[3][2] == token && @board[2][2] == token ||
+      @board[4][2] == token && @board[3][2] == token && @board[2][2] == token && @board[1][2] == token ||
+      @board[3][2] == token && @board[2][2] == token && @board[1][2] == token && @board[0][2] == token ||
+      @board[5][3] == token && @board[4][3] == token && @board[3][3] == token && @board[2][3] == token ||
+      @board[4][3] == token && @board[3][3] == token && @board[2][3] == token && @board[1][3] == token ||
+      @board[3][3] == token && @board[2][3] == token && @board[1][3] == token && @board[0][3] == token ||
+      @board[5][4] == token && @board[4][4] == token && @board[3][4] == token && @board[2][4] == token ||
+      @board[4][4] == token && @board[3][4] == token && @board[2][4] == token && @board[1][4] == token ||
+      @board[3][4] == token && @board[2][4] == token && @board[1][4] == token && @board[0][4] == token ||
+      @board[5][5] == token && @board[4][5] == token && @board[3][5] == token && @board[2][5] == token ||
+      @board[4][5] == token && @board[3][5] == token && @board[2][5] == token && @board[1][5] == token ||
+      @board[3][5] == token && @board[2][5] == token && @board[1][5] == token && @board[0][5] == token ||
+      @board[5][6] == token && @board[4][6] == token && @board[3][6] == token && @board[2][6] == token ||
+      @board[4][6] == token && @board[3][6] == token && @board[2][6] == token && @board[1][6] == token ||
+      @board[3][6] == token && @board[2][6] == token && @board[1][6] == token && @board[0][6] == token ||
+      @board[5][0] == token && @board[4][1] == token && @board[3][2] == token && @board[2][3] == token ||
+      @board[5][1] == token && @board[4][2] == token && @board[3][3] == token && @board[2][4] == token ||
+      @board[5][2] == token && @board[4][3] == token && @board[3][4] == token && @board[2][5] == token ||
+      @board[5][3] == token && @board[4][4] == token && @board[3][5] == token && @board[2][6] == token ||
+      @board[4][0] == token && @board[3][1] == token && @board[2][2] == token && @board[1][3] == token ||
+      @board[4][1] == token && @board[3][2] == token && @board[2][3] == token && @board[1][4] == token ||
+      @board[4][2] == token && @board[3][3] == token && @board[2][4] == token && @board[1][5] == token ||
+      @board[4][3] == token && @board[3][4] == token && @board[2][5] == token && @board[1][6] == token ||
+      @board[3][0] == token && @board[2][1] == token && @board[1][2] == token && @board[0][3] == token ||
+      @board[3][1] == token && @board[2][2] == token && @board[1][3] == token && @board[0][4] == token ||
+      @board[3][2] == token && @board[2][3] == token && @board[1][4] == token && @board[0][5] == token ||
+      @board[3][3] == token && @board[2][4] == token && @board[1][5] == token && @board[0][6] == token ||
+      @board[5][6] == token && @board[4][5] == token && @board[3][4] == token && @board[2][3] == token ||
+      @board[5][5] == token && @board[4][4] == token && @board[3][3] == token && @board[2][2] == token ||
+      @board[5][4] == token && @board[4][3] == token && @board[3][2] == token && @board[2][1] == token ||
+      @board[5][3] == token && @board[4][2] == token && @board[3][1] == token && @board[2][0] == token ||
+      @board[4][6] == token && @board[3][5] == token && @board[2][4] == token && @board[1][3] == token ||
+      @board[4][5] == token && @board[3][4] == token && @board[2][3] == token && @board[1][2] == token ||
+      @board[4][4] == token && @board[3][3] == token && @board[2][2] == token && @board[1][1] == token ||
+      @board[4][3] == token && @board[3][2] == token && @board[2][1] == token && @board[1][0] == token ||
+      @board[3][6] == token && @board[2][5] == token && @board[1][4] == token && @board[0][3] == token ||
+      @board[3][5] == token && @board[2][4] == token && @board[1][3] == token && @board[0][2] == token ||
+      @board[3][4] == token && @board[2][3] == token && @board[1][2] == token && @board[0][1] == token ||
+      @board[3][3] == token && @board[2][2] == token && @board[1][1] == token && @board[0][0] == token
   end
 
   def game_draw?
@@ -112,7 +113,7 @@ class ConnectFour
 
   def place_token(column_number, token)
     if filled?(@board[0][column_number - 1])
-      'Error, please choose a column that is not full already'
+      'Error, please choose a column that is not full already.'
     elsif filled?(@board[1][column_number - 1])
       @board[0][column_number - 1] = token
     elsif filled?(@board[2][column_number - 1])
@@ -131,20 +132,37 @@ class ConnectFour
   def filled?(co_ord)
     return true unless co_ord == '_'
   end
+
+  def player_turn(player)
+    puts "#{player.name}, please pick a column to place your token in: "
+    column_number = gets.chomp
+    until validate?(column_number)
+      puts 'Please choose a number from 1 - 7.'
+      column_number = gets.chomp
+    end
+    place_token(column_number.to_i, player.token)
+  end
+
+  def validate?(input)
+    return true if input.match?(/^[1-7]$/)
+  end
+
+  def play_game
+    loop do
+      player_turn(@player_number_one)
+      display
+      @player_number_one.score += 1 if game_win?(@player_number_one.token)
+      break if game_over?(@player_number_one.token)
+      player_turn(@player_number_two)
+      display
+      @player_number_two.score += 1 if game_win?(@player_number_two.token)
+      break if game_over?(@player_number_two.token)
+    end
+    puts "The winner is #{@player_number_one.score > @player_number_two.score ? @player_number_one.name : @player_number_two.name}"
+    # Ask if user wants to play again?
+  end
 end
 
 game = ConnectFour.new
 
-puts game.display
-
-game.place_token(1, 'X')
-
-puts game.display
-
-game.place_token(1, 'X')
-
-puts game.display
-
-game.place_token(1, 'X')
-
-puts game.display
+game.play_game
--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,35 @@
 # Frozen_string_literal: true
 
 # Class defining player class and methods for interacting with ConnectFour game class
-
 class Player
-  attr_reader :name, :player_number
+  attr_reader :name, :player_number, :token
+  attr_accessor :score
 
-  def initialize(name, player_number)
-    @name = name
+  def initialize(player_number)
     @player_number = player_number
+    @name = ask_name
+    @token = ask_token
+    @score = 0
+  end
+
+  def ask_name
+    puts "Player #{@player_number}, please enter your name: "
+    @name = gets.chomp.capitalize
+  end
+
+  def ask_token
+    puts "Now choose a token to play with by choosing a number "
+    puts "1: X"
+    puts "2: O"
+    choice = gets.chomp
+    until validate?(choice)
+      puts 'Please choose either 1 or 2'
+      choice = gets.chomp
+    end
+    choice == '1' ? @token = 'X' : @token = 'O'
+  end
+
+  def validate?(input)
+    return true if input.match?(/^[12]$/)
   end
 end
